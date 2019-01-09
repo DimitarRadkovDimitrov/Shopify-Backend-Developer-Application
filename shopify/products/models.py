dcab9f1015370ec98391e8adb65ac7c8c92a0a21
Dimitar Dimitrov <dimitar.a.dimitrov@ericsson.com>
2019-01-09 13:56:15 -0500

    added different queries for products

--- shopify/products/models.py
+++ shopify/products/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
 class Product(models.Model):
+    id = models.AutoField(primary_key=True)
     title = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=10)
     inventory_count = models.IntegerField()
+
+    def __str__(self):
+        return self.title
--- shopify/products/schema.py
+++ shopify/products/schema.py
@@ -10,7 +10,27 @@ class ProductType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    products = graphene.List(ProductType)
+    product = graphene.Field(ProductType, id=graphene.Int(), title=graphene.String())
+    all_products = graphene.List(ProductType, inventory_count_gt=graphene.Int())
 
-    def resolve_products(self, info, **kwargs):
+    def resolve_product(self, info, **kwargs):
+        """ Query for individual product, id always takes precedence over title """
+
+        id = kwargs.get('id')
+        title = kwargs.get('title')
+
+        if id is not None:
+            return Product.objects.get(id=id)
+        
+        if title is not None:
+            return Product.objects.get(title=title)
+
+    def resolve_all_products(self, info, **kwargs):
+        """ Query for all products in db, optional argument for product inventories greater than x """
+
+        inventory_gt = kwargs.get('inventory_count_gt')
+        
+        if inventory_gt is not None:
+            return Product.objects.all().filter(inventory_count__gt=inventory_gt)
+
         return Product.objects.all()
